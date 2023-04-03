91fb5f3ba82a8b270b5a6caaa2dab3b2e85470b6
Rick Lupton <mail@ricklupton.name>
2023-04-03 23:17:22 +0100

    Simplify __main__ to just print blocks
    Use `rmc` or another tool for proper command line use.

--- src/rmscene/__main__.py
+++ src/rmscene/__main__.py
@@ -2,29 +2,12 @@
 
 import sys
 import argparse
-from . import read_blocks, write_blocks, TextFormat
-from .text import extract_text, simple_text_document
+from . import read_blocks
 
 
 def parse_args(args):
-    # create the top-level parser
     parser = argparse.ArgumentParser(prog="rmscene")
-    # parser.add_argument('--foo', action='store_true', help='foo help')
-
-    subparsers = parser.add_subparsers(help="sub-command help")
-
-    parser_a = subparsers.add_parser("print-blocks", help="dump scene block data")
-    parser_a.add_argument("file", type=argparse.FileType("rb"), help="filename to read")
-    parser_a.set_defaults(func=pprint_file)
-
-    parser_b = subparsers.add_parser("print-text", help="dump text")
-    parser_b.add_argument("file", type=argparse.FileType("rb"), help="filename to read")
-    parser_b.set_defaults(func=print_text)
-
-    parser_c = subparsers.add_parser("text2rm", help="convert text")
-    parser_c.add_argument("file", type=argparse.FileType("wb"), help="filename to write")
-    parser_c.set_defaults(func=convert_text)
-
+    parser.add_argument("file", type=argparse.FileType("rb"), help="filename to read")
     return parser.parse_args(args)
 
 
@@ -37,26 +20,6 @@ def pprint_file(args) -> None:
         pprint.pprint(el)
 
 
-def print_text(args):
-    for fmt, line in extract_text(args.file):
-        if fmt == TextFormat.BULLET:
-            print("- " + line)
-        elif fmt == TextFormat.BULLET2:
-            print("  + " + line)
-        elif fmt == TextFormat.BOLD:
-            print("> " + line)
-        elif fmt == TextFormat.HEADING:
-            print("# " + line)
-        elif fmt == TextFormat.PLAIN:
-            print(line)
-        else:
-            print(("[unknown format %s] " % fmt) + line)
-
-
-def convert_text(args):
-    write_blocks(args.file, simple_text_document(sys.stdin.read()))
-
-
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
-    args.func(args)
+    pprint_file(args)
